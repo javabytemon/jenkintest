9b550a7bf3010ee351c7a1773d9a647a57fa0ee0
Chike <kivilactech@gmail.com>
2023-07-20 15:42:13 +0100

    init

---
+++ src/com/bytelogic/jenkins/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.bytelogic.jenkins;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is a jenkins test\n Lets say hello world");
+    }
+}
